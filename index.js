b5faafcbbf4dc56696cd8fe5a59612ae9792c132
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-04-28 18:35:23 -0400

    commit tree cad57959b283136126439270cad80d5c998611d9

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//has a function expression called divide. Divide divides 2000 by 100
+const divide = function divide(num1, num2){
+    return (2000 / 100)
+}
+
+//has an arrow function called square.  square arrow function takes one parameter and multiplies it times itself
+const square = (num1) => num1 * num1
+
+
+//has an arrow function called add. add arrow function takes two parameters and adds them together (a + b)
+const add = (num3, num4) => num3 + num4 
